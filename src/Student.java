4a32f6d5b305dea9a4134bfce6c4e0a2cebafc69
майор <ucebydla910@gmail.com>
2026-02-06 12:31:37 +0500

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

7161493bae60c8257a7bd5e7a418fcfc848f60e1
майор <ucebydla910@gmail.com>
2026-02-06 12:32:10 +0500

    Update StudentController.java

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name) {
+        model.setName(name);
+    }
+
+    public String getStudentName() {
+        return model.getName();
+    }
+
+    public void setStudentAge(int age) {
+        model.setAge(age);
+    }
+
+    public int getStudentAge() {
+        return model.getAge();
+    }
+
+    public void updateView() {
+        view.printStudentDetails(model.getName(), model.getAge());
+    }
 }

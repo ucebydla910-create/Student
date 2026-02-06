424a8bfe1fe3bd35c4397f7375986ccc973694da
User <User@WIN-7RSVVMUHAJG>
2026-02-06 12:30:32 +0500

    Initial commit

---
+++ src/StudentView.java
@@ -0,0 +1,7 @@
+public class StudentView {
+    public void printStudentDetails(String name, int age) {
+        System.out.println("Студент:");
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+    }
+}

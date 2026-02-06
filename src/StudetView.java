cd858f4fe10915738d4eb610dcb40082fd50d50f
майор <ucebydla910@gmail.com>
2026-02-06 12:32:29 +0500

    Update StudetView.java

--- src/StudetView.java
+++ src/StudetView.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class StudetView {
+public class StudentView {
+    public void printStudentDetails(String name, int age) {
+        System.out.println("Студент:");
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+    }
 }
